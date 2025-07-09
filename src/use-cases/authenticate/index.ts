53d150126208a8a6973504946519f0bcee1c7ca5
Sílvio Contreiras <silviocontreiras@MacBook-Pro-de-Silvio.local>
2025-07-09 10:03:00 -0300

    feat: add auth

---
+++ src/use-cases/authenticate/index.ts
@@ -0,0 +1,36 @@
+import { UsersRepository } from '@/repositories/users'
+import { IvalidCredentialsError } from '../errors/invalid-credentials.error'
+import { compare } from 'bcryptjs'
+import { User } from '@prisma/client'
+
+interface AuthenticateUseCaseRequest {
+   email: string
+   password: string 
+}
+
+interface AuthenticateUseCaseResponse {
+    user: User
+}
+
+export class AuthenticateUseCase {
+    constructor(private usersRepository: UsersRepository) {}
+
+    async execute({ email, password }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+        const user = await this.usersRepository.findByEmail(email)
+
+        if (!user) {
+            throw new IvalidCredentialsError()
+        }
+
+
+        const isValidPassword = await compare(password, user.password_hash)
+
+        if (!isValidPassword) {
+            throw new IvalidCredentialsError()
+        }
+
+        return {
+            user
+        }
+    }
+}
---
+++ src/use-cases/errors/invalid-credentials.error.ts
@@ -0,0 +1,6 @@
+export class IvalidCredentialsError extends Error {
+    constructor() {
+      super('Invalid credentials error.')
+    }
+  }
+  
