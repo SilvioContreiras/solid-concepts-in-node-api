25995477c1bdb40922085ab825c0babe5370f6eb
Sílvio Contreiras <silviocontreiras@MacBook-Pro-de-Silvio.local>
2025-08-06 06:01:05 -0300

    feat(checkin): add usecase

--- src/use-cases/check-in/check-in.spec.ts
+++ src/use-cases/check-in/check-in.spec.ts
@@ -1,21 +1,62 @@
-import { expect, describe, it, beforeEach } from 'vitest'
-import { CheckInUseCase } from './'
-import { InMemoryCheckInRepository } from '@/repositories/in-memory/in-memory-check-in.repository'
+import { expect, describe, it, beforeEach, vi, afterEach } from "vitest";
+import { CheckInUseCase } from "./";
+import { InMemoryCheckInRepository } from "@/repositories/in-memory/in-memory-check-in.repository";
 
-let usersRepository: InMemoryCheckInRepository
-let checkInUseCase: CheckInUseCase
+let usersRepository: InMemoryCheckInRepository;
+let checkInUseCase: CheckInUseCase;
 
-describe('Check-in Use Case', () => {
+describe("Check-in Use Case", () => {
   beforeEach(() => {
-    usersRepository = new InMemoryCheckInRepository()
-    checkInUseCase = new CheckInUseCase(usersRepository)
-  })
-  it('should create a Check-in', async () => {
-    const { checkIn } = await checkInUseCase.execute({
-      gymId: 'gym-01',
-      userId: 'user-01',
-    })
+    usersRepository = new InMemoryCheckInRepository();
+    checkInUseCase = new CheckInUseCase(usersRepository);
 
-    expect(checkIn.id).toEqual(expect.any(String))
-  })
-})
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it("should create a Check-in", async () => {
+    vi.setSystemTime(new Date(2025, 9, 10, 8, 0, 0));
+
+    const { checkIn } = await checkInUseCase.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+    });
+
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it("should not make checkin twice in the same day", async () => {
+    await checkInUseCase.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+    });
+
+    await expect(() =>
+      checkInUseCase.execute({
+        gymId: "gym-01",
+        userId: "user-01",
+      })
+    ).rejects.toBeInstanceOf(Error);
+  });
+
+  it("should allow check-in in differents dates", async () => {
+    vi.setSystemTime(new Date(2025, 9, 10, 8, 0, 0));
+
+    await checkInUseCase.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+    });
+
+    vi.setSystemTime(new Date(2025, 9, 11, 8, 0, 0));
+
+    const { checkIn } = await checkInUseCase.execute({
+        gymId: "gym-01",
+        userId: "user-01",
+      });
+  
+      expect(checkIn.id).toEqual(expect.any(String));
+  });
+});
--- src/use-cases/check-in/index.ts
+++ src/use-cases/check-in/index.ts
@@ -14,6 +14,11 @@ export class CheckInUseCase {
     constructor(private checkInRepository: CheckInRepository) {}
 
     async execute({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+        const checkInOnSameDate = await this.checkInRepository.findByUserId(userId, new Date())
+        if (checkInOnSameDate) {
+            throw new Error('You can only check in once per day.')
+        }
+        
         const checkIn = await this.checkInRepository.create({
             user_id: userId,
             gym_id: gymId,
